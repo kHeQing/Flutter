06449d4ff069fba77a00168fbdae5c506af4a545
heqing <245377839@qq.com>
2019-05-20 11:04:12 +0800

    GridView Widget 网格组件

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # Flutter
 学习flutter记录
 
+### Flutter 常用组件
+
 - 文本组件 Text Widget
 - 容器组件 Contain Widget
 - 图片组件 Image Widget
-- 列表组件 List Widget
+- 列表组件 ListView Widget
+- 网格组件 GridView Widget
+
+### Flutter 布局
+
--- flutter_widget/lib/main.dart
+++ flutter_widget/lib/main.dart
@@ -1,4 +1,4 @@
-// ListView Widget 2 横向
+// GridView Widget
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,41 +7,127 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ListView Widget',
+      title: 'GridView Widget',
       home: Scaffold(
-        body: Center(
-          child: Container(
-            height: 200.0,
-            child: MyList()
+        // body: GridView.count(
+        //   padding: EdgeInsets.all(20.0),
+        //   crossAxisSpacing: 10.0,
+        //   crossAxisCount: 3,
+        //   children: <Widget>[
+        //     Text('he qing'),
+        //     Text('今天是个好日子'),
+        //     Text('心想的事儿都能成'),
+        //     Text('今天明天都是好日子'),
+        //     Text('咱们老百姓'),
+        //     Text('今天真高兴'),
+        //   ],
+        // ),
+        body: GridView(
+          padding: EdgeInsets.all(2.0),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            mainAxisSpacing: 2.0,
+            childAspectRatio: 0.7, // 宽高比
+            crossAxisSpacing: 2.0,
           ),
+          children: <Widget>[
+            Image.network('http://pic25.nipic.com/20121205/10197997_003647426000_2.jpg',fit: BoxFit.fitHeight,),
+            Image.network('https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1986179278,1118313821&fm=27&gp=0.jpg',fit: BoxFit.fitHeight,),
+            Image.network('https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1979198230,3799788659&fm=27&gp=0.jpg',fit: BoxFit.fitHeight,),
+            Image.network('https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3300305952,1328708913&fm=27&gp=0.jpg',fit: BoxFit.fitHeight,),
+            Image.network('https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2153937626,1074119156&fm=27&gp=0.jpg',fit: BoxFit.fitHeight,),
+            Image.network('https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2968231382,1111539856&fm=27&gp=0.jpg',fit: BoxFit.fitHeight,),
+            Image.network('https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1451330793,2242997567&fm=27&gp=0.jpg',fit: BoxFit.fitHeight,),
+          ],
         ),
       ),
     );
   }
 }
 
-class MyList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      scrollDirection: Axis.horizontal,
-              children: <Widget>[
-                Container(
-                  width: 180.0,
-                  color: Colors.greenAccent,
-                ),
-                Container(
-                  width: 180.0,
-                  color: Colors.lightBlue,
-                ),
-                Container(
-                  width: 180.0,
-                  color: Colors.pinkAccent,
-                ),
-              ],
-    );
-  }
-}
+
+
+
+
+// // Dynamic List Widget
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(MyApp(
+//   items: List<String>.generate(1000, (i) => 'Item $i')
+// ));
+
+// class MyApp extends StatelessWidget {
+
+//   final List<String> items;
+//   MyApp({Key key, @required this.items}):super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'ListView Widget',
+//       home: Scaffold(
+//         body: ListView.builder(
+//           itemCount: items.length,
+//           itemBuilder: (context, index) {
+//             return ListTile(
+//               title: Text('${items[index]}'),
+//             );
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+// // ListView Widget 2 横向
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'ListView Widget',
+//       home: Scaffold(
+//         body: Center(
+//           child: Container(
+//             height: 200.0,
+//             child: MyList()
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class MyList extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListView(
+//       scrollDirection: Axis.horizontal,
+//         children: <Widget>[
+//           Container(
+//             width: 180.0,
+//             color: Colors.greenAccent,
+//           ),
+//           Container(
+//             width: 180.0,
+//             color: Colors.lightBlue,
+//           ),
+//           Container(
+//             width: 180.0,
+//             color: Colors.pinkAccent,
+//           ),
+//         ],
+//     );
+//   }
+// }
+
+
 
 
 // // ListView Widget 1 竖向
@@ -77,6 +163,7 @@ class MyList extends StatelessWidget {
 //     );
 //   }
 // }
+
 
 
 
